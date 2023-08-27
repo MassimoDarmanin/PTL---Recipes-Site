bc75c1b72862865e5778a487098544b0e7a0668b
MassimoDarmanin <50630749+MassimoDarmanin@users.noreply.github.com>
2023-08-27 14:42:35 +0200

    Setting up DAL models
    Connected with new DB PTLRecipes using "Scaffold-DbContext "Server=(local); DataBase=PTLRecipes;Integrated Security=true" Microsoft.EntityFrameworkCore.SqlServer -OutPutDir Models"

---
+++ PTL - Recipe Site/App.DAL/DataContext/PTLRecipesContext.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using App.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace App.DAL.DataContext
+{
+    public partial class PTLRecipesContext : DbContext
+    {
+        public PTLRecipesContext()
+        {
+        }
+
+        public PTLRecipesContext(DbContextOptions<PTLRecipesContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Ingredient> Ingredients { get; set; } = null!;
+        public virtual DbSet<Recipe> Recipes { get; set; } = null!;
+        public virtual DbSet<RecipeIngredient> RecipeIngredients { get; set; } = null!;
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Ingredient>(entity =>
+            {
+                entity.Property(e => e.IngredientId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("IngredientID");
+
+                entity.Property(e => e.Category).HasMaxLength(50);
+
+                entity.Property(e => e.Name).HasMaxLength(100);
+
+                entity.Property(e => e.Unit).HasMaxLength(20);
+            });
+
+            modelBuilder.Entity<Recipe>(entity =>
+            {
+                entity.Property(e => e.RecipeId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("RecipeID");
+
+                entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Description).HasMaxLength(500);
+
+                entity.Property(e => e.Title).HasMaxLength(100);
+
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Recipes)
+                    .HasForeignKey(d => d.UserId)
+                    .HasConstraintName("FK__Recipes__UserID__398D8EEE");
+            });
+
+            modelBuilder.Entity<RecipeIngredient>(entity =>
+            {
+                entity.HasKey(e => new { e.RecipeId, e.IngredientId })
+                    .HasName("PK__RecipeIn__463363F7AD33E2EE");
+
+                entity.Property(e => e.RecipeId).HasColumnName("RecipeID");
+
+                entity.Property(e => e.IngredientId).HasColumnName("IngredientID");
+
+                entity.Property(e => e.Quantity).HasColumnType("decimal(10, 2)");
+
+                entity.HasOne(d => d.Ingredient)
+                    .WithMany(p => p.RecipeIngredients)
+                    .HasForeignKey(d => d.IngredientId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__RecipeIng__Ingre__3F466844");
+
+                entity.HasOne(d => d.Recipe)
+                    .WithMany(p => p.RecipeIngredients)
+                    .HasForeignKey(d => d.RecipeId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__RecipeIng__Recip__3E52440B");
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.UserId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("UserID");
+
+                entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Email).HasMaxLength(100);
+
+                entity.Property(e => e.Username).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ PTL - Recipe Site/App.DAL/Models/Ingredient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App.DAL.Models
+{
+    public partial class Ingredient
+    {
+        public Ingredient()
+        {
+            RecipeIngredients = new HashSet<RecipeIngredient>();
+        }
+
+        public int IngredientId { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Category { get; set; }
+        public string? Unit { get; set; }
+
+        public virtual ICollection<RecipeIngredient> RecipeIngredients { get; set; }
+    }
+}
