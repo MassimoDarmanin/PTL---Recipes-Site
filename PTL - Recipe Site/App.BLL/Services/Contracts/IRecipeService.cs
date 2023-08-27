6930b9b4b8020c7b0b8c595fb15d0f3da2ec86b8
MassimoDarmanin <50630749+MassimoDarmanin@users.noreply.github.com>
2023-08-27 15:30:35 +0200

    Init Recipe Service
    Created generic Services for Recipes and changed startup project to UI

---
+++ PTL - Recipe Site/App.BLL/Services/Contracts/IRecipeService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using App.DAL.Models;
+
+namespace App.BLL.Services.Contracts
+{
+    internal interface IRecipeService
+    {
+        Task<List<Recipe>> GetRecipes();
+    }
+}
---
+++ PTL - Recipe Site/App.BLL/Services/RecipeService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using App.DAL.Models;
+using App.DAL.Repositories.Contracts;
+using App.BLL.Services.Contracts;
+
+namespace App.BLL.Services
+{
+    public class RecipeService : IRecipeService
+    {
+        private readonly IGenericRepository<Recipe> _repository;
+
+        public RecipeService(IGenericRepository<Recipe> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<Recipe>> GetRecipes()
+        {
+            try
+            {
+                return await _repository.GetRecipes();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
