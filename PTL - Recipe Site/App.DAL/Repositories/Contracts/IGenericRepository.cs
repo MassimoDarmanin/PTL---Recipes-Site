af3becb587da7d5b2814eeff19aa1ff2c8a9ccf2
MassimoDarmanin <50630749+MassimoDarmanin@users.noreply.github.com>
2023-08-27 15:10:17 +0200

    Setting Up Generic Repositories

--- PTL - Recipe Site/App.DAL/Repositories/Contracts/IGenericRepository.cs
+++ PTL - Recipe Site/App.DAL/Repositories/Contracts/IGenericRepository.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 using App.DAL.Models;
 namespace App.DAL.Repositories.Contracts
 {
-    public interface IGenericRepository
+    public interface IGenericRepository<TModel> where TModel : class
     {
+        Task<List<TModel>> GetRecipes();
     }
 }
---
+++ PTL - Recipe Site/App.DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+using App.DAL.DataContext;
+using App.DAL.Models;
+using App.DAL.Repositories.Contracts;
+
+namespace App.DAL.Repositories
+{
+    public class GenericRepository<TModel> : IGenericRepository<TModel> where TModel : class
+    {
+        private readonly PTLRecipesContext _recipesContext;
+
+        public GenericRepository(PTLRecipesContext pTLRecipesContext)
+        {
+            _recipesContext = pTLRecipesContext;
+        }
+
+        public async Task<List<TModel>> GetRecipes()
+        {
+            try
+            {
+                return await _recipesContext.Set<TModel>().ToListAsync();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
